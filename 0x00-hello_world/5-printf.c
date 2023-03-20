4eb2b17cb99f6d5666f9bfc57aa47fbd861a5556
root <simo.karkouz.2001@gmail.com>
2023-03-20 03:07:15 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* *
+ * main - a program that print  a line using function print
+ *
+ * return: 0 (sucesse)
+ */
+int main (void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
