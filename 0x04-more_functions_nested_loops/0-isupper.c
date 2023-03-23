c6d02587c6b651f17366739b996cecc0b02db81f
root <simo.karkouz.2001@gmail.com>
2023-03-23 12:10:55 -0700

    fonction

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,3 @@
-#include "main.h"
-/**
- * _isupper - checks for uppercase character
- * @c: the character to be checked
- *
- * Return: 1 if c is uppercase, 0 otherwise
- */
-int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
-}
-
+[master cb09d3f] 0-isupper.c
+ 2 files changed, 18 insertions(+), 16 deletions(-)
+ rewrite 0x04-more_functions_nested_loops/README.md (100%)
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,6 @@
-[master 1ce0c69] fonction
- 2 files changed, 16 insertions(+), 15 deletions(-)
- create mode 100755 0x04-more_functions_nested_loops/README.md
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+int _isupper(int c);
+#endif /* MAIN_H */
+
